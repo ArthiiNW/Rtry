5ee3b415a6f6a8070ce013f36037898a09302865
ArthiiNW <xhs315@alumni.ku.dk>
2021-09-21 13:50:56 +0200

    2

---
+++ random  script.R
@@ -0,0 +1,9 @@
+# WOW I still have that sds192 repository
+# wow someone just jumped down
+# command shift m
+%>% %>% 
+  # wow!!
+  
+# read_delim: can costume delimiter
+# and read_csv2//read.csv2
+  
